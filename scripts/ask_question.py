da581e346d15369d346c58e46d95d48cb4026f31
Nimesh <connectnv01@gmail.com>
2026-04-03 13:00:37 +0530

    "modified"

--- scripts/ask_question.py
+++ scripts/ask_question.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Add project root to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pipelines.rag_pipeline import run_rag
